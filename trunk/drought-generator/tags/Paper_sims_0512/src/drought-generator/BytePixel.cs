b850c35e835f13538a114a9a26e3405b422d3f8d
Brian <brmirand@gmail.com>
2012-08-09 18:36:37 +0000

    Importing drought-generator code (official release 1.0)
    git-svn-id: http://Marc-PC/svn/Full@748 85f15e0e-5137-b144-a863-98bd542365a3

---
+++ trunk/drought-generator/tags/Paper_sims_0512/src/drought-generator/BytePixel.cs
@@ -0,0 +1,14 @@
+﻿using Landis.SpatialModeling;
+
+namespace Landis.Extension.DroughtGenerator
+{
+    public class BytePixel : Pixel
+    {
+        public Band<byte> MapCode = "The numeric code for each raster cell";
+
+        public BytePixel()
+        {
+            SetBands(MapCode);
+        }
+    }
+}
