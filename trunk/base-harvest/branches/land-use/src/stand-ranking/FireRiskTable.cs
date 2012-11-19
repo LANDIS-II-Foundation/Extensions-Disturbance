eb77f924833cae959114d5dc2e36778fb109fb7a
jimm <jdomingo@green-code.com>
2012-11-19 12:37:52 +0000

    Created branch for Base Harvest version that's land-use aware.
    git-svn-id: http://Marc-PC/svn/Full@861 85f15e0e-5137-b144-a863-98bd542365a3

---
+++ trunk/base-harvest/branches/land-use/src/stand-ranking/FireRiskTable.cs
@@ -0,0 +1,39 @@
+//  Copyright 2005-2010 Portland State University, University of Wisconsin
+//  Authors:  Robert M. Scheller, James B. Domingo
+
+using Landis.Core;
+
+namespace Landis.Extension.BaseHarvest
+{
+    /// <summary>
+    /// A collection of parameters for computing the economic ranks of species.
+    /// </summary>
+    public class FireRiskTable
+    {
+        private FireRiskParameters[] parameters;
+
+        //---------------------------------------------------------------------
+
+        public FireRiskParameters this[int fuelTypeIndex]
+        {
+            get {
+                return parameters[fuelTypeIndex];
+            }
+
+            set {
+                parameters[fuelTypeIndex] = value;
+            }
+        }
+
+        //---------------------------------------------------------------------
+
+        public FireRiskTable()
+        {
+            parameters = new FireRiskParameters[150];  //up to 150 fuel types
+            //foreach (FireRiskParameters fireRiskParm in parameters)
+            //{
+            //    fireRiskParm 
+            //}
+        }
+    }
+}
