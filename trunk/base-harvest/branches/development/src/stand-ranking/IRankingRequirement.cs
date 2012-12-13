15aeb8eeb700a067ab05eb514d35ae77300b1014
jimm <jdomingo@green-code.com>
2012-12-13 19:57:56 +0000

    Created a branch for a custom Base Development extension for JT. This quick & dirty hack is based on cloning Base Harvest.
    git-svn-id: http://Marc-PC/svn/Full@878 85f15e0e-5137-b144-a863-98bd542365a3

---
+++ trunk/base-harvest/branches/development/src/stand-ranking/IRankingRequirement.cs
@@ -0,0 +1,14 @@
+namespace Landis.Extension.BaseHarvest
+{
+    /// <summary>
+    /// A requirement that a stand must meet in order to be eligible for
+    /// ranking.
+    /// </summary>
+    public interface IRankingRequirement
+    {
+        /// <summary>
+        /// Does a stand meet the requirement?
+        /// </summary>
+        bool MetBy(Stand stand);
+    }
+}
