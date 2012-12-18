51f67f0a8abd820c4279292e2811186cac0cc9c7
jimm <jdomingo@green-code.com>
2012-12-18 16:47:01 +0000

    Tagged beta 2 release of Base Development version 0.1. Released on 20 November 2012.
    git-svn-id: http://Marc-PC/svn/Full@890 85f15e0e-5137-b144-a863-98bd542365a3

---
+++ trunk/base-harvest/tags/development/0.1b2/src/stand-ranking/StandRanking.cs
@@ -0,0 +1,11 @@
+namespace Landis.Extension.BaseHarvest
+{
+    /// <summary>
+    /// A stand and its ranking.
+    /// </summary>
+    public struct StandRanking
+    {
+        public Stand Stand;
+        public double Rank;
+    }
+}
