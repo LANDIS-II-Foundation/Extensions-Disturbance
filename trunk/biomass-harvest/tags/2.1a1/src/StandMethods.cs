8e16bd9f956e6c07f31c81ecf0df027ae4289699
jimm <jdomingo@green-code.com>
2013-01-08 02:04:26 +0000

    Tagged the alpha 1 release of version 2.1 (event-log feature branch). Released on 19 December 2012.
    git-svn-id: http://Marc-PC/svn/Full@924 85f15e0e-5137-b144-a863-98bd542365a3

---
+++ trunk/biomass-harvest/tags/2.1a1/src/StandMethods.cs
@@ -0,0 +1,56 @@
+﻿using Landis.Core;
+using Landis.Extension.BaseHarvest;
+using System.Collections.Generic;
+
+namespace Landis.Extension.BiomassHarvest
+{
+    public static class StandMethods
+    {
+        // Keys are stand map codes; values are mapping of species' names and biomass totals
+        private static IDictionary<uint, IList<int>> biomassRemovedBySpecies;
+
+        //--------------------------------------------------------------------
+
+        static void Initialize()
+        {
+            biomassRemovedBySpecies = new Dictionary<uint, IList<int>>();
+        }
+
+        //--------------------------------------------------------------------
+
+        public static void RecordBiomassRemoved(this Stand stand,
+                                                ISpecies species,
+                                                int reduction)
+        {
+            if (biomassRemovedBySpecies == null)
+                Initialize();
+            if (biomassRemovedBySpecies[stand.MapCode] == null)
+                biomassRemovedBySpecies[stand.MapCode] = new List<int>(Model.Core.Species.Count);
+
+            biomassRemovedBySpecies[stand.MapCode][species.Index] += reduction;
+        }
+
+        //--------------------------------------------------------------------
+
+        public static int GetBiomassRemoved(this Stand stand,
+                                            ISpecies species)
+        {
+            if (biomassRemovedBySpecies == null)
+                Initialize();
+            return biomassRemovedBySpecies[stand.MapCode][species.Index];
+        }
+
+        //--------------------------------------------------------------------
+
+        public static void ResetBiomassRemoved(this Stand stand)
+        {
+            if (biomassRemovedBySpecies == null)
+                Initialize();
+            foreach (IList<int> speciesTotals in biomassRemovedBySpecies.Values)
+            {
+                for (int i = 0; i < speciesTotals.Count; i++)
+                    speciesTotals[i] = 0;
+            }
+        }
+    }
+}
