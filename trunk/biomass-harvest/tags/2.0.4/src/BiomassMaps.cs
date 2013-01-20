85e274c60b33cc39c0d142be9e6af8a82873f602
jimm <jdomingo@green-code.com>
2013-01-20 17:51:47 +0000

    Tagged version 2.0.4 of Biomass Harvest. Released 9 October 2012 (this is latest release available on L-II web site today).
    git-svn-id: http://Marc-PC/svn/Full@977 85f15e0e-5137-b144-a863-98bd542365a3

---
+++ trunk/biomass-harvest/tags/2.0.4/src/BiomassMaps.cs
@@ -0,0 +1,53 @@
+// Copyright 2008-2010 Green Code LLC, Portland State University
+// Authors:  James B. Domingo, Robert M. Scheller,  
+ 
+using Landis.SpatialModeling;
+using System;
+using Landis.Extension.BaseHarvest;
+
+namespace Landis.Extension.BiomassHarvest
+{
+    /// <summary>
+    /// Utility class for writing biomass-removed maps.
+    /// </summary>
+    public class BiomassMaps
+    {
+        private string nameTemplate;
+
+        //---------------------------------------------------------------------
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="nameTemplate">
+        /// The template for the pathnames to the maps.
+        /// </param>
+        public BiomassMaps(string nameTemplate)
+        {
+            this.nameTemplate = nameTemplate;
+        }
+
+        //---------------------------------------------------------------------
+
+        /// <summary>
+        /// Writes an output map of biomass removed from each active site.
+        /// </summary>
+        /// <param name="timestep">
+        /// Timestep to use in the map's name.
+        /// </param>
+        public void WriteMap(int timestep)
+        {
+            string path = BaseHarvest.MapNames.ReplaceTemplateVars(nameTemplate, timestep);
+            PlugIn.ModelCore.Log.WriteLine("   Writing biomass-removed map to {0} ...", path);
+            using (IOutputRaster<IntPixel> outputRaster = PlugIn.ModelCore.CreateRaster<IntPixel>(path, PlugIn.ModelCore.Landscape.Dimensions))
+            {
+                IntPixel pixel = outputRaster.BufferPixel;
+                foreach (Site site in PlugIn.ModelCore.Landscape.AllSites) {
+                    pixel.MapCode.Value = (int) Math.Round(SiteVars.BiomassRemoved[site]);  
+                    outputRaster.WriteBufferPixel();
+                }
+            }
+        }
+
+    }
+}
