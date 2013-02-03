ebefd0a862b4352afc2d7ad7cb3630aeb8dcc8bb
jimm <jdomingo@green-code.com>
2013-02-03 01:00:33 +0000

    Tagged release candidate 1 of Base Development 0.1
    git-svn-id: http://Marc-PC/svn/Full@1018 85f15e0e-5137-b144-a863-98bd542365a3

---
+++ trunk/base-harvest/tags/development/0.1rc1/src/stand-ranking/MaximumAge.cs
@@ -0,0 +1,26 @@
+namespace Landis.Extension.BaseHarvest
+{
+    /// <summary>
+    /// A ranking requirement which requires a stand be no more than a certain
+    /// maximum age to be eligible for ranking.
+    /// </summary>
+    public class MaximumAge
+        : IRequirement
+    {
+        private ushort maxAge;
+
+        //---------------------------------------------------------------------
+
+        public MaximumAge(ushort age)
+        {
+            maxAge = age;
+        }
+
+        //---------------------------------------------------------------------
+
+        bool IRequirement.MetBy(Stand stand)
+        {
+            return stand.Age <= maxAge;
+        }
+    }
+}
